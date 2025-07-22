7ef365a94c2eb59844261b84ce853379e0426c65
shreyagrawal09 <agrawalshrey02@gmail.com>
2025-07-22 16:37:56 +0530

    initial commit

---
+++ classses.java
@@ -0,0 +1,6 @@
+public class classses {
+    public static void main(String[] args) {
+        System.out.println("shrey");
+        System.out.println("Hello, World!");
+    }
+}
---
+++ newClasss.java
@@ -0,0 +1,5 @@
+public class newClasss {
+    public static void main(String[] args) {
+        System.out.println("shrey");
+    }
+}
